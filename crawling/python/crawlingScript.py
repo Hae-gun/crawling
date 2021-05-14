330147108158f98665a4357c81bfa7f249e1dff9
최혜근 <@DESKTOP-CT2B6I5>
2021-05-14 17:54:20 +0900

    commit

--- crawling/python/crawlingScript.py
+++ crawling/python/crawlingScript.py
@@ -2,6 +2,7 @@ import requests
 import pandas
 from bs4 import BeautifulSoup
 import json
+import time
 def crawling(url):
     req = requests.get(url,verify=False)
     print('Crawling Finish')
@@ -14,20 +15,52 @@ result=crawling(url)
 reform=result.select(
     '#problemset > tbody > tr > td:nth-child(2) > a'
     )
+print('url>>> ',url)
+def allCrawling(url,lastnumber):
+    myDict=dict()
+    for i in range(1,lastnumber):
+        reformUrl=url+'/'+str(i)
+        req = requests.get(reformUrl,verify=False)
+        soup = BeautifulSoup(req.text,'html.parser')
+        reform=soup.select(
+            '#problemset > tbody > tr > td:nth-child(2) > a'
+            )
+        level=soup.select(
+            '#problemset > tbody > tr > td:nth-child(2) > a > span'
+            )
+        print(level)
+        print(reform)
+        for data in reform:
+            sub=str(data)
+            start=sub.find('>')
+            #print(start)
+            end=sub.rfind('<')
+            name=sub[start+1:end]
+            subUrl=sub[1:start]
+            myDict[name]=subUrl
+        time.sleep(1)
+    return myDict
 
-myDict=dict()
-for data in reform:
-    sub=str(data)
-    start=sub.find('>')
-    #print(start)
-    end=sub.rfind('<')
-    name=sub[start+1:end]
-    url=sub[1:start]
-    myDict[name]=url
+#lastNumber=result.select(
+#    '/html/body/div[2]/div[2]/div[6]/div[2]/div/ul/li[208]/a'
+#    )
+#print(lastNumber)
+result=allCrawling(url,2)
 
-json_val=json.dumps(myDict,ensure_ascii=False,indent='\t')
-print(json_val)
-
-with open('result.json','w',encoding='utf-8') as make_file:
-    json.dump(myDict,make_file,ensure_ascii=False,indent='\t')
+print('result>>> ',result)
+#myDict=dict()
+#for data in reform:
+#    sub=str(data)
+#    start=sub.find('>')
+#    #print(start)
+#    end=sub.rfind('<')
+#    name=sub[start+1:end]
+#    url=sub[1:start]
+#    myDict[name]=url
+##
+#json_val=json.dumps(myDict,ensure_ascii=False,indent='\t')
+#print(json_val)
+#
+#with open('result.json','w',encoding='utf-8') as make_file:
+#    json.dump(result,make_file,ensure_ascii=False,indent='\t')
 
