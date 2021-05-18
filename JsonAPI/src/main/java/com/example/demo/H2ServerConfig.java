c4453814c27ae9eaf9b61e10f0758a63541ef028
최혜근 <@DESKTOP-CT2B6I5>
2021-05-18 13:55:45 +0900

    del

--- JsonAPI/src/main/java/com/example/demo/H2ServerConfig.java
+++ JsonAPI/src/main/java/com/example/demo/H2ServerConfig.java
@@ -7,16 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.repository.TestRepository;
-import com.example.repository.TestRepositoryImpl;
 
 @Configuration
 public class H2ServerConfig {
 	@Bean
 	public Server h2Tcpserver() throws SQLException {
 		return Server.createTcpServer().start();
-	}
-	@Bean
-	public TestRepository testRepository() throws SQLException {
-		return new TestRepositoryImpl(h2Tcpserver());
 	}
 }
